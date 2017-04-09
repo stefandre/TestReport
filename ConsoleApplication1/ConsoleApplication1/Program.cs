4afd0d7cd258d136b3519be45704f08f8e56c6a9
Andreas.stefanou <stefanouand@gmail.com>
2017-04-09 20:43:58 +0300

    Final

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -7,11 +7,36 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Configuration;
 using System.Data.Common;
+using System.IO;
 
 namespace ConsoleApplication1
 {
     class Program
     {
+        public static void Write(DataTable dt, string filePath)
+        {
+            int i = 0;
+            StreamWriter sw = null;
+            sw = new StreamWriter(filePath, false);
+            for (i = 0; i < dt.Columns.Count - 1; i++)
+            {
+                sw.Write(dt.Columns[i].ColumnName + " ");
+            }
+            sw.Write(dt.Columns[i].ColumnName);
+            sw.WriteLine();
+            foreach (DataRow row in dt.Rows)
+            {
+                object[] array = row.ItemArray;
+                for (i = 0; i < array.Length - 1; i++)
+                {
+                    sw.Write(array[i] + " ");
+                }
+                sw.Write(array[i].ToString());
+                sw.WriteLine();
+            }
+            sw.Close();
+        }
+
         static void Main(string[] args)
         {
             try
@@ -39,8 +64,17 @@ namespace ConsoleApplication1
                         Console.WriteLine(item); // Invokes ToString abstract method.
                     }
                 }
-
                 Console.ReadLine();
+
+                string filePath = ConfigurationManager.AppSettings["filePath"] + ConfigurationManager.AppSettings["fileName"];
+
+                if (File.Exists(filePath))
+                {
+                    string e = "The file " + filePath + " already exists.";
+                    throw new Exception(e);
+                }
+                
+                Write(dt, filePath );
                 con.Conn.Dispose();
 
                 //// execute the command
