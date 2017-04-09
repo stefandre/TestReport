21ce337e17625d1044d9849b9de4835ade6f657f
Andreas.stefanou <stefanouand@gmail.com>
2017-04-09 19:56:50 +0300

    All commits

---
+++ ConsoleApplication1/ConsoleApplication1/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+    class Connection
+    {
+
+        private string sSQL;
+        private SqlConnection conn;
+
+        public SqlConnection Conn { get; set; }
+
+        public string SSQL { get; set; }
+
+        //
+    }
+}
